d6071e5f8f60d4004f9833b9d5de1ef549eed764
DemonsAH <demonsah@outlook.com>
2025-07-20 16:40:22 +0200

    init commit(gitignore set)

---
+++ share/update_lkp_codes.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 设置路径变量
+SRC=~/share/lab6/*
+DST=~/linux/lkp/
+LKPDIR=~/linux
+
+# 拷贝文件
+cp -r ~/share/lab7/* ~/linux/lkp/
+
+# 进入内核源目录
+cd "$LKPDIR" || exit 1
+
+# 添加文件到 git
+git add lkp
+#git add fs
+#git add include
+
+# 使用 --amend 修改上一次提交（进入编辑器，可根据需要修改提交信息）
+git commit --amend --no-edit
+
+# 发送邮件补丁（基于 HEAD~1）
+git send-email --no-cc --to lkp-maintainers@os.rwth-aachen.de HEAD~1
