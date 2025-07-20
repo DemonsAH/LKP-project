462d7ffb33869aa7f1273971de56476945b8d0f6
DemonsAH <demonsah@outlook.com>
2025-07-20 19:19:43 +0200

    update update shell

--- share/update_lkp_codes.sh
+++ share/update_lkp_codes.sh
@@ -6,18 +6,18 @@ DST=~/linux/lkp/
 LKPDIR=~/linux
 
 # 拷贝文件
-cp -r ~/share/lab7/* ~/linux/lkp/
+cp -r ~/share/project/* ~/linux/ouichefs/
 
 # 进入内核源目录
-cd "$LKPDIR" || exit 1
+#cd "$LKPDIR" || exit 1
 
 # 添加文件到 git
-git add lkp
+#git add lkp
 #git add fs
 #git add include
 
 # 使用 --amend 修改上一次提交（进入编辑器，可根据需要修改提交信息）
-git commit --amend --no-edit
+#git commit --amend --no-edit
 
 # 发送邮件补丁（基于 HEAD~1）
-git send-email --no-cc --to lkp-maintainers@os.rwth-aachen.de HEAD~1
+#git send-email --no-cc --to lkp-maintainers@os.rwth-aachen.de HEAD~1
